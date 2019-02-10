e4ea5a33aaa32810626a9707ee9de706c9f6796f
Thomas Shehan <thomas.shehan88@gmail.com>
2019-02-10 14:12:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,101 @@
+/* eslint-env jquery*/
+$(startUp);
+let $main;
+
+function startUp() {
+  $main = $('main');
+  navigate('nav0');
+  addListeners();
+}
+
+function addListeners() {
+  const $nav = $('nav');
+  $nav.on('click', 'a', function() {
+    const id = $(this).attr('id');
+    navigate(id);
+    navSelect(id);
+  });
+}
+
+function navigate(id) {
+  switch (id) {
+    case 'nav0':
+      render0();
+      break;
+    case 'nav1':
+      render1();
+      break;
+    case 'nav2':
+      render2();
+      break;
+    default:
+  }
+}
+
+function navSelect(id) {
+  $('.nav-open').removeClass('nav-open');
+  $(`#${id}`).addClass('nav-open');
+}
+
+function render0() {
+  $main.empty();
+  const $section = $('<section>', {
+    class: 'js-sec0',
+    html: $('<p>', {
+      text: `Highly motivated web developer, with experience building RESTful APIs,
+        managing databases, and building applications with Javascript,
+        React, and various testing suites.`
+    }),
+  });
+  $main.append($section);
+}
+
+function render1() {
+  $main.empty();
+  const $section = $('<section>', {
+    class: 'js-sec1',
+    html: [
+      $('<article>', {
+        html: $('<a>', {
+          href: "https://github.com/tkshehan/chess-openings",
+          text: 'Lichess Openings',
+        })
+      }),
+      $('<article>', {
+        html: $('<a>', {
+          href: "https://github.com/tkshehan/tekken-7-frames",
+          text: 'Tekken 7 Frame Data API',
+        })
+      }),
+      $('<article>', {
+        html: $('<a>', {
+          href: "https://github.com/tkshehan/packing-planner",
+          text: 'Simple Packing',
+        })
+      }),
+    ],
+  });
+  $main.append($section);
+}
+
+function render2() {
+  $main.empty();
+  const $section = $('<section>', {
+    class: 'js-sec2',
+    html: [
+      $('<a>', {
+        href: 'https://www.linkedin.com/in/thomaskshehan/',
+        text: 'LinkedIn',
+      }),
+      $('<a>', {
+        href: 'https://github.com/tkshehan',
+        text: 'GitHub',
+      }),
+      $('<a>', {
+        href: 'mailto:thomas.shehan88@gmail.com',
+        text: 'Email',
+      }),
+    ],
+  });
+  $main.append($section);
+}
