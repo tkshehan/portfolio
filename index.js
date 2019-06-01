21f03c478405bb27318075d71eebac24e197c574
Thomas Shehan <thomas.shehan88@gmail.com>
2019-05-31 20:43:16 -0400

    update projects to include descriptions

--- index.js
+++ index.js
@@ -41,13 +41,16 @@ function render0() {
   $main.empty();
   const $section = $('<section>', {
     class: 'js-sec0 about',
-    html: $('<p>', {
+    html: [$('<p>', {
       text: `
-        Highly motivated web developer focused on customer satisfaction, with
-        experience building RESTful APIs,
-        managing databases, and building applications with HTML/CSS, Javascript,
-        React, and various testing suites.`
+        Enthusiastic web developer with a love of problem solving, a focus on clean, manageable code, client oriented service and drive to help others succeed.`
     }),
+    $('<p>', {
+      text: `Experience building RESTful APIs,
+        managing databases, and building applications with HTML/CSS, Javascript,
+        React, and assorted testing suites.`
+    }),
+    ],
   });
   $main.append($section);
 }
@@ -57,24 +60,9 @@ function render1() {
   const $section = $('<section>', {
     class: 'js-sec1 projects',
     html: [
-      $('<article>', {
-        html: $('<a>', {
-          href: "https://github.com/tkshehan/chess-openings",
-          text: 'Lichess Openings',
-        })
-      }),
-      $('<article>', {
-        html: $('<a>', {
-          href: "https://github.com/tkshehan/t7-client",
-          text: 'Tekken 7 Frame Data',
-        })
-      }),
-      $('<article>', {
-        html: $('<a>', {
-          href: "https://github.com/tkshehan/packing-planner",
-          text: 'Simple Packing',
-        })
-      }),
+      project1(),
+      project2(),
+      project3(),
     ],
   });
   $main.append($section);
@@ -100,4 +88,75 @@ function render2() {
     ],
   });
   $main.append($section);
+}
+
+function project1() {
+  return $('<article>', {
+    html: [
+      $('<a>', {
+        href: "https://github.com/tkshehan/t7-client",
+        text: 'Tekken 7 Frame Data',
+      }),
+      $('<ul>', {
+        html: [
+          $('<li>', {
+            text: `Lightweight web-crawler to regularly retrieve frame data from reliable sources`,
+          }),
+          $('<li>', {
+            text: `API in Express.js/Node.js providing up to date frame data to myself and other developers.`,
+          }),
+          $('<li>', {
+            text: `Front-end React/Redux webapp continuously under development.`,
+          }),
+        ],
+      }),
+    ],
+  });
+}
+
+function project2() {
+  return $('<article>', {
+    html: [
+      $('<a>', {
+        href: "https://github.com/tkshehan/chess-openings",
+        text: 'Lichess Openings',
+      }),
+      $('<ul>', {
+        html: [
+          $('<li>', {
+            text: `An accessibility focused static web application for Lichess.org users to gather data on their past games.`,
+          }),
+          $('<li>', {
+            text: `Mobile-first, responsive design, following a11y best practices.`,
+          }),
+          $('<li>', {
+            text: `Additional resources integrated from multiple outside sources for the convenience of the end-user.`,
+          }),
+        ],
+      }),
+    ],
+  });
+}
+
+function project3() {
+  return $('<article>', {
+    html: [$('<a>', {
+      href: "https://github.com/tkshehan/packing-planner",
+      text: 'Simple Packing',
+    }),
+    $('<ul>', {
+      html: [
+        $('<li>', {
+          text: `A mobile-first web app for planning, saving, and printing checklists for frequent travelers.`,
+        }),
+        $('<li>', {
+          text: `Features mongodb, express, express-routers, and delivered through browserify for a modulated development environment.`,
+        }),
+        $('<li>', {
+          text: `Continuous integration and testing through Travis-CI.`,
+        }),
+      ],
+    }),
+    ],
+  });
 }
